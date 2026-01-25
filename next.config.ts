d4cb69cf92b114228e0412182098c6b0e57aecdb
goofbrush <goofbrush@gmail.com>
2026-01-25 16:17:41 +0000

    fix3?

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  basePath: "/swrSite",
+  assetPrefix: "/swrSite/"
 };
 
 export default nextConfig;
